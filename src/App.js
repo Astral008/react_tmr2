d59c09fab80b330602b84f0c359441041b4f9860
Astral008 <102272420+Astral008@users.noreply.github.com>
2023-04-10 00:42:43 -0500

    en proceso

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
+import React from 'react';
+
+const todos=[
+  {text:'cortar cebolla', completed:false},
+  {text:'comer', completed:false},
+  {text:'llorar', completed:false}
+]
 
 function App() {
   return (
-    // <TodoCounter/>
-    // <TodoSearch/>
-    // <TodoList>
-    //   <TodoItem/>
-    <h1>hola</h1>
-    // </TodoList>
-    // <CreateButon/>
-    // <Button/>
-  );
+    <React.Fragment>
+      {/* <TodoCounter/> */}
+    <h2>Has completado 2 de 3 TODOs</h2>
+    {/* <TodoSearch/> */}
+    <input placeholder="Cebolla"/>
+    {/* <TodoList>
+      {todos.map(todo=>(
+        <TodoItem/>
+      ))} */}
+    
+    {/* </TodoList>
+    <CreateTodoBuuton/>
+    <Button/> */}
+  
+    </React.Fragment>
+    );
 }
 
 export default App;
